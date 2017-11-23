c657fba607623081d3d1f8c67cd23e98ed9d1e55
David McPike <7037366+mcdafydd@users.noreply.github.com>
2017-11-23 04:36:32 -0600

    update start scripts for cockpit - force restart of camera mqtt daemon; move sleep and renice to end

--- start-prod.sh
+++ start-prod.sh
@@ -9,10 +9,20 @@ TEMP_URL='http://'$EXTERNAL_CAM_IP'/i2c.php?width=8&bus=1&adr=0x4800'
 SET_PARAMS_URL='http://'$EXTERNAL_CAM_IP'/parsedit.php?immediate&AUTOEXP_ON=0&QUALITY=80&EXPOS=30000'
 curl $SET_PARAMS_URL
 
+# Restart all instances of mqttclient.paho (inittab will respawn)
+curl -v --connect-timeout 5 'http://192.168.2.211/phpshell.php?command=killall%20mqttclient.paho'
+curl -v --connect-timeout 5 'http://192.168.2.212/phpshell.php?command=killall%20mqttclient.paho'
+curl -v --connect-timeout 5 'http://192.168.2.213/phpshell.php?command=killall%20mqttclient.paho'
+curl -v --connect-timeout 5 'http://192.168.2.215/phpshell.php?command=killall%20mqttclient.paho'
+curl -v --connect-timeout 5 'http://192.168.2.217/phpshell.php?command=killall%20mqttclient.paho'
+
 # Kill any mjpg_streamers hanging around before renice
 killall -9 mjpg_streamer
-sudo -n renice -n -1 -p `sleep 10; ps auxww|grep mjpg_streamer | awk '{print $2}'` &
 
 # Always use this in production
 USE_MOCK=false EXTERNAL_CAM=true EXTERNAL_CAM_URL='http://'$EXTERNAL_CAM_IP':8081/bmimg' NODE_ENV='production' PLATFORM='scini' BOARD='surface' HARDWARE_MOCK=false DEV_MODE=true cacheDirectory='/tmp/cache' DATADIR='/tmp' LOG_LEVEL='warn' IGNORE_CACHE=true configfile='/tmp/rovconfig.json' pluginsDownloadDiretory='/tmp/plugins' photoDirectory="/tmp" video_url='http://'$EXTERNAL_CAM_IP':8081/bmimg' env plugins__ui-manager__selectedUI='new-ui' node src/cockpit.js
+
+# Wait 10 seconds and then renice mjpg_streamer to -1
+sleep 10
+sudo -n renice -n -1 -p `ps auxww|grep mjpg_streamer | awk '{print $2}'` &
 
