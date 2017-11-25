54e3882872c3ca0fff1588d91e96aab64a07efea
David McPike <7037366+mcdafydd@users.noreply.github.com>
2017-11-24 19:33:19 -0600

    temporarily remove renice

--- start-prod.sh
+++ start-prod.sh
@@ -7,7 +7,7 @@ TEMP_URL='http://'$EXTERNAL_CAM_IP'/i2c.php?width=8&bus=1&adr=0x4800'
 # Attempt to set defaults on camera
 # Autoexposure off; JPEG 80% quality; Exposure 30ms
 SET_PARAMS_URL='http://'$EXTERNAL_CAM_IP'/parsedit.php?immediate&AUTOEXP_ON=0&QUALITY=80&EXPOS=30000'
-curl $SET_PARAMS_URL
+curl -v --connect-timeout 5 $SET_PARAMS_URL
 
 # Restart all instances of mqttclient.paho (inittab will respawn)
 curl -v --connect-timeout 5 'http://192.168.2.211/phpshell.php?command=killall%20mqttclient.paho'
@@ -20,9 +20,9 @@ curl -v --connect-timeout 5 'http://192.168.2.218/phpshell.php?command=killall%2
 killall -9 mjpg_streamer
 
 # Always use this in production
-USE_MOCK=false EXTERNAL_CAM=true EXTERNAL_CAM_URL='http://'$EXTERNAL_CAM_IP':8081/bmimg' NODE_ENV='production' PLATFORM='scini' BOARD='surface' HARDWARE_MOCK=false DEV_MODE=true cacheDirectory='/tmp/cache' DATADIR='/tmp' LOG_LEVEL='warn' IGNORE_CACHE=true configfile='/tmp/rovconfig.json' pluginsDownloadDiretory='/tmp/plugins' photoDirectory="/tmp" video_url='http://'$EXTERNAL_CAM_IP':8081/bmimg' env plugins__ui-manager__selectedUI='new-ui' node src/cockpit.js &
+USE_MOCK=false EXTERNAL_CAM=true EXTERNAL_CAM_URL='http://'$EXTERNAL_CAM_IP':8081/bmimg' NODE_ENV='production' PLATFORM='scini' BOARD='surface' HARDWARE_MOCK=false DEV_MODE=true cacheDirectory='/tmp/cache' DATADIR='/tmp' LOG_LEVEL='warn' IGNORE_CACHE=true configfile='/tmp/rovconfig.json' pluginsDownloadDiretory='/tmp/plugins' photoDirectory="/tmp" video_url='http://'$EXTERNAL_CAM_IP':8081/bmimg' env plugins__ui-manager__selectedUI='new-ui' node src/cockpit.js 
 
 # Wait 10 seconds and then renice mjpg_streamer to -1
-sleep 10
-sudo -n renice -n -1 -p `ps auxww|grep mjpg_streamer | awk '{print $2}'` &
+#sleep 10
+#sudo -n renice -n -1 -p `ps auxww|grep mjpg_streamer | awk '{print $2}'` && fg %1
 
